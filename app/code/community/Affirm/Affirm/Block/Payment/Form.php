b0c17e70970866f00710e8df2dff55abe5ecf089
Brian Holder-Chow Lin On <brian@affirm.com>
2014-04-24 18:59:29 +0000

    update checkout flow messaging at payment information view
    See: https://affirm.atlassian.net/browse/DEV-1014
    
    replace payment method image with Affirm card
    
    modify the text displayed when Affirm is selected

--- app/code/community/Affirm/Affirm/Block/Payment/Form.php
+++ app/code/community/Affirm/Affirm/Block/Payment/Form.php
@@ -1,9 +1,42 @@
 <?php
 class Affirm_Affirm_Block_Payment_Form extends Mage_Payment_Block_Form
 {
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->replaceLabel();
+    }
+
     protected function _toHtml()
     {
-        $method = $this->getMethod();
-        return "Put it on my affirm tab!";
+      // TODO(brian): extract this html block to a template
+      $msg = "You will be sent to the Affirm website to complete your payment.";
+      $html = "<ul class=\"form-list\" id=\"payment_form_affirm\" style=\"display:none;\">";
+      $html .= "<li class=\"form-alt\">" . $msg . "</li>";
+      $html .= "</ul>";
+      return $html;
     }
-}   
+
+    /* Replaces default label with custom image, conditionally displaying text
+     * based on the Affirm product.
+     *
+     * Context: Payment Information step of Checkout flow
+     */
+    private function replaceLabel()
+    {
+        $this->setMethodTitle(""); // removes default title
+
+        // TODO(brian): extract html to template
+        $logoSrc = Mage::getDesign()->getSkinUrl('images/affirm/affirm-card.png');
+        $html = "<img src=\"" . $logoSrc . "\" class=\"v-middle\" />&nbsp;";
+
+        // TODO(brian): conditionally display based on payment type
+        if (true) {
+          $html.= "Buy Now and Pay Later";
+        } else {
+          $html.= "Buy with monthly payments";
+        }
+
+        $this->setMethodLabelAfterHtml($html);
+    }
+}
--- dev/link.sh
+++ dev/link.sh
@@ -8,4 +8,5 @@
 # TODO(brian): make idempotent
 # TODO(brian): make it correct when called from any directory
 ln -s -t $*/app/code/community/ $(cd .. && pwd)/app/code/community/Affirm/
+ln -s -t $*/skin/frontend/base/default/images/ $(cd .. && pwd)/skin/frontend/base/default/images/affirm/
 ln -s $(cd .. && pwd)/app/etc/modules/Affirm_Affirm.xml $*/app/etc/modules 
