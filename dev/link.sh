1368c4ed401647e1f2690fb7c26af605ca005634
Brian Holder-Chow Lin On <brian@affirm.com>
2014-04-24 18:33:57 +0000

    add script to link source to a magento installation
    script must be updated when new directories/files are added to source repo

---
+++ dev/link.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Usage: sh /link.sh /absolute/path/to/magento/root/
+
+# Example: sh link.sh /home/bitnami/apps/magento/htdocs/
+
+# given absolute path to magento root directory, create symbolic links to source code
+# TODO(brian): make idempotent
+# TODO(brian): make it correct when called from any directory
+ln -s -t $*/app/code/community/ $(cd .. && pwd)/app/code/community/Affirm/
+ln -s $(cd .. && pwd)/app/etc/modules/Affirm_Affirm.xml $*/app/etc/modules 
