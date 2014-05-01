70153b88b782f5edfd1a5a668842c63111eca03e
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2014-05-01 12:31:10 -0700

    move extension code to extension dir

--- dev/link.sh
+++ dev/link.sh
@@ -7,6 +7,8 @@
 # given absolute path to magento root directory, create symbolic links to source code
 # TODO(brian): make idempotent
 # TODO(brian): make it correct when called from any directory
-ln -s -t $*/app/code/community/ $(cd .. && pwd)/app/code/community/Affirm/
-ln -s -t $*/skin/frontend/base/default/images/ $(cd .. && pwd)/skin/frontend/base/default/images/affirm/
-ln -s $(cd .. && pwd)/app/etc/modules/Affirm_Affirm.xml $*/app/etc/modules 
+# TODO(brian): de-duplicate finding extension dir 
+
+ln -s -t $*/app/code/community/ $(cd ../extension && pwd)/app/code/community/Affirm/
+ln -s -t $*/skin/frontend/base/default/images/ $(cd ../extension && pwd)/skin/frontend/base/default/images/affirm/
+ln -s $(cd ../extension && pwd)/app/etc/modules/Affirm_Affirm.xml $*/app/etc/modules 
