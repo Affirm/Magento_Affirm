57381967802aab5aa66b0d5d2abdf59ae57ae507
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2014-05-19 14:32:35 -0700

    [lib] replace formatCents impl to fix bug
    see:
    https://github.com/Affirm/affirm-php/commit/c9ceb2b82111290bc429cf215063b1b92cbf1293

--- extension/lib/Affirm/Affirm/Util.php
+++ extension/lib/Affirm/Affirm/Util.php
@@ -1,7 +1,46 @@
 <?php
 
 class Affirm_Util {
-    public static function formatCents($amount = 0) {
-        return (int) ($amount * 100);
+
+    //  .2   -  right precision
+    //  f    -  as float
+    const MONEY_FORMAT = "%.2f";
+
+    public static function formatMoney($amount) {
+        return sprintf(static::MONEY_FORMAT, $amount);
     }
+
+    // TODO rename format as string
+    public static function formatCents($amount = 0) {
+        $negative = false;
+
+        $str = static::formatMoney($amount);
+
+        if (strcmp($str[0], "-") === 0) {
+            // treat it like a positive. then prepend a '-' to the return value.
+            $str = substr($str, 1);
+            $negative = true;
+        }
+
+        $parts = explode(".", $str, 2);
+        if ($parts === false) {
+            return 0;
+        }
+
+        if (empty($parts)) {
+            return 0;
+        }
+
+        if (strcmp($parts[0], 0) === 0 && strcmp($parts[1], "00") === 0) {
+            return 0;
+        }
+
+        $retVal = "";
+        if ($negative) {
+            $retVal .= "-";
+        }
+        $retVal .= ltrim( $parts[0] . substr($parts[1], 0, 2), "0");
+        return intval($retVal);
+    }
+
 }
