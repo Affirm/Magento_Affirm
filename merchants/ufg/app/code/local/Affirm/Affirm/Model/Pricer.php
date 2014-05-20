4b00c160e2cfe9e68dd4281a5d32418b31d3a962
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2014-05-20 12:20:39 -0700

    add UFG custom pricer
    impl for ufg
    
    update pricer implementation
    
    [bugfix] assign return value as list literal
    
    Conflicts:
    	merchants/ufg/app/code/local/Affirm/Affirm/Model/Pricer.php

--- merchants/ufg/app/code/local/Affirm/Affirm/Model/Pricer.php
+++ merchants/ufg/app/code/local/Affirm/Affirm/Model/Pricer.php
@@ -1,10 +1,31 @@
 <?php
 
+/*
+ * Pricer for UFG's custom price model for bundled products.
+ */
 class Affirm_Affirm_Model_Pricer
 {
-    // TODO(brian): impl for UFG
     public function getPrice($order_item)
     {
+        if ($order_item->getProductType() ===
+            Mage_Catalog_Model_Product_Type::TYPE_BUNDLE)
+        {
+            $bundle_product = $this->_getProduct($order_item);
+            list($list_price, $our_price) =
+                $bundle_product->getPriceModel()->getTypicalPrices($bundle_product);
+            return $our_price;
+        }
         return $order_item->getPrice();
+    }
+
+    private function _getProduct($order_item)
+    {
+        // TODO(brian): OPTIMIZE consider keeping a handle as an instance
+        // variable. This is expensive to call for each item.
+        $products = Mage::getModel('catalog/product');
+
+        $options = $order_item->getProductOptions();
+        $productId = $options["info_buyRequest"]["product"];
+        return $products->load($productId);
     }
 }
