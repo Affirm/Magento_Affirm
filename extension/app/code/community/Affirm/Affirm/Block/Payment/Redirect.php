1c38465f0e169513c3e5bcea7a25e6745cd29c00
Manuel Arias <manny@affirm.com>
2014-05-22 13:15:51 -0700

    Subscribe to error close event for redirect
    When affirm error UI closes we redirect to the checkout url.

--- extension/app/code/community/Affirm/Affirm/Block/Payment/Redirect.php
+++ extension/app/code/community/Affirm/Affirm/Block/Payment/Redirect.php
@@ -8,7 +8,11 @@ class Affirm_Affirm_Block_Payment_Redirect extends Mage_Core_Block_Abstract
 
         $html = '<html><body>';
         $html.= '<script type="text/javascript" src="'. trim($payment_method->getBaseApiUrl(), "/") . '/js/v2/affirm.js"></script>';
-        $html.= '<script type="text/javascript">affirm.checkout(' . json_encode($payment_method->getCheckoutObject($order)) . '); affirm.checkout.post();</script>';
+        $html.= '<script type="text/javascript">';
+        $html.= 'affirm.checkout(' . json_encode($payment_method->getCheckoutObject($order)) . ');';
+        $html.= 'affirm.ui.error.on("close", function(){ window.location= "' . Mage::helper('checkout/url') . '";});';
+        $html.= 'affirm.checkout.post();';
+        $html.= '</script>';
         $html.= '</body></html>';
         return $html;
     }
