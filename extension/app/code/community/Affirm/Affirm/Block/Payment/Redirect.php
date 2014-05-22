cd204040d8a377e0b1327d7f26865850da9a8176
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2014-05-22 15:40:45 -0700

    [fix] return string

--- extension/app/code/community/Affirm/Affirm/Block/Payment/Redirect.php
+++ extension/app/code/community/Affirm/Affirm/Block/Payment/Redirect.php
@@ -10,7 +10,7 @@ class Affirm_Affirm_Block_Payment_Redirect extends Mage_Core_Block_Abstract
         $html.= '<script type="text/javascript" src="'. trim($payment_method->getBaseApiUrl(), "/") . '/js/v2/affirm.js"></script>';
         $html.= '<script type="text/javascript">';
         $html.= 'affirm.checkout(' . json_encode($payment_method->getCheckoutObject($order)) . ');';
-        $html.= 'affirm.ui.error.on("close", function(){ window.location= "' . Mage::helper('checkout/url') . '";});';
+        $html.= 'affirm.ui.error.on("close", function(){ window.location= "' . Mage::helper('checkout/url')->getCheckoutUrl() . '";});';
         $html.= 'affirm.checkout.post();';
         $html.= '</script>';
         $html.= '</body></html>';
