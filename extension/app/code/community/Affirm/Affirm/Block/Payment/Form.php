b911318e678b22dba68c54b2fae44543159d2cd6
Brian Holder-Chow Lin On <brian@affirm.com>
2014-08-15 12:14:43 -0700

    [style] update copy

--- extension/app/code/community/Affirm/Affirm/Block/Payment/Form.php
+++ extension/app/code/community/Affirm/Affirm/Block/Payment/Form.php
@@ -57,7 +57,7 @@ class Affirm_Affirm_Block_Payment_Form extends Mage_Payment_Block_Form
 
             // TODO(brian): conditionally display based on payment type
             // alt message: $html.= "Buy Now and Pay Later";
-            $html.= "Split into 3 Easy Payments";
+            $html.= "Buy Now with 3 Easy Payments";
 
             $this->setMethodLabelAfterHtml($html);
         }
