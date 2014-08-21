e497f2d928fcb93c0be7955f79e2e8b21ed09ba5
Brian Holder-Chow Lin On <brian@affirm.com>
2014-08-21 16:51:45 -0700

    [chore] bump version

--- build/affirm_tar_to_connect_config.php
+++ build/affirm_tar_to_connect_config.php
@@ -4,8 +4,8 @@ return array(
 'archive_files'        => 'Affirm_Magento.tar',
 'extension_name'       => 'Affirm_Magento',
 'skip_version_compare' => true,
-'extension_version'    => '2.0.5',
-'archive_connect'      => 'Affirm_Magento-2.0.5.tgz',
+'extension_version'    => '2.0.6',
+'archive_connect'      => 'Affirm_Magento-2.0.6.tgz',
 'path_output'          => realpath('../var/'),
 
 'stability'            => 'stable',
