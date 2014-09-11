0ed0d76da1c2a06744751984d89def1575f5e4b8
garrett-schlesinger <garrett.schlesinger@affirm.com>
2014-09-11 16:50:57 -0700

    [promos] add all of the affirm promo sizes as options; change default promo settings

--- extension/app/code/community/Affirm/AffirmPromo/Model/System/Config/Size.php
+++ extension/app/code/community/Affirm/AffirmPromo/Model/System/Config/Size.php
@@ -4,7 +4,24 @@ class Affirm_AffirmPromo_Model_System_Config_Size extends Varien_Object
 {
     public static $sizes = array(
         "196x193",
-        "560x63"
+        "560x63",
+        "120x90",
+        "150x100",
+        "170x100",
+        "190x100",
+        "234x60",
+        "300x50",
+        "468x60",
+        "300x250",
+        "336x280",
+        "540x200",
+        "728x90",
+        "800x66",
+        "250x250",
+        "280x280",
+        "120x240",
+        "120x600",
+        "234x400"
     );
 
     /**
