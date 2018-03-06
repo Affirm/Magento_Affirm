dba5d2c1c545f705049ddb40a20905f69b5638ec
Nanda <nanda.dubey@affirm.com>
2018-03-05 22:49:24 -0800

    Admin notification for extension updates and version displayed on admin

---
+++ extension/app/code/community/Affirm/Affirm/Block/Adminhtml/Version.php
@@ -0,0 +1,8 @@
+<?php
+class Affirm_Affirm_Block_Adminhtml_Version extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
+    {
+        return (string) Mage::helper('affirm')->getExtensionVersion();
+    }
+}
