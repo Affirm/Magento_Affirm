7be8a66aa54735763eb860f2e6722b21e8efccf7
Nanda <nanda.dubey@affirm.com>
2018-03-08 11:57:36 -0800

    updates due to merge issues

--- extension/app/code/community/Affirm/Affirm/Model/Adminhtml/Observer.php
+++ extension/app/code/community/Affirm/Affirm/Model/Adminhtml/Observer.php
@@ -3,19 +3,17 @@ class Affirm_Affirm_Model_Adminhtml_Observer
 {
     /**
      * Predispatch action controller
+     * @param Varien_Event_Observer $observer
      */
-    public function preDispatch()
+    public function preDispatch(Varien_Event_Observer $observer)
     {
-        Mage::log(__METHOD__);
         /* @var $session Mage_Admin_Model_Session */
         $session = Mage::getSingleton('admin/session');
         if ($session->isLoggedIn()) {
-            $feedModel = Mage::getModel(
-                'affirm/feed'
-            );
+            $feedModel = Mage::getModel('affirm/feed');
             /* @var $feedModel Mage_AdminNotification_Model_Feed */
             $feedModel->checkUpdate();
         }
     }
-    
+
 }
